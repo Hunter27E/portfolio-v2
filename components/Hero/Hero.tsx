01aa64f73fa6d23b6a1657f75a48c6e19ecafbe5
Hunter27E <50163607+Hunter27E@users.noreply.github.com>
2024-07-18 21:48:45 -0700

    Fix hero font

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
 
 import { Anton } from '@next/font/google'
+const anton = Anton({
+	subsets: ['latin'],
+	weight: ['400'],
+	style: ['normal'],
+	variable: '--font-anton',
+})
 
 import styles from './Hero.module.scss'
 
-const anton = Anton({ subsets: ['latin'], weight: ['400'], style: ['normal'] })
-
 export const Hero = () => {
 	return (
-		<section className={styles.hero}>
+		<section className={`${styles.hero} ${anton.variable}`}>
 			<h2 className={styles.subtitle}>software engineer</h2>
-			<h1 className={`${styles.title} outlineText ${anton.className}`}>
-				HUNTER ESTRADA
-			</h1>
+			<h1 className={`${styles.title} outlineText anton`}>HUNTER ESTRADA</h1>
 			<div className={styles.ctaBtns}>
 				<a href='#contactMe' className='btnPrimary'>
 					SAY HELLO
